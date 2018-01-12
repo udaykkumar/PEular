9f3c91ed8f8f96178c7a3692265c14134b0c7a5a
Uday <k.udaykumar1985@gmail.com>
2018-01-12 22:23:58 +0530

    Problem-10 init file

---
+++ problem-10.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+'''
+
+import time
+
+
+def PrimesSum(n):
+    return "Hello"
+
+start_time = time.time()
+print PrimesSum(2000000)
+print("--- %s seconds ---" % (time.time() - start_time))
