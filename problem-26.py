897f72139734d8acea3fb7e15510c891c4b96923
Uday <k.udaykumar1985@gmail.com>
2018-01-27 18:32:44 +0530

    Still a TODO for problem 26 in search of a better approach

--- problem-26.py
+++ problem-26.py
@@ -23,52 +23,38 @@ import sys
 
 
 def recurringSequence(n):
-    rec = {}
-    d = 1
-    i = 1
-    '''
-        Some self talk:
-            Assumption is recurring digits are not more than 9 digits thats considering the fact that if any of digits 1 - 9 repeats same cycle repeats.
-            Thus below algorithm proceeds as below
+    i = 0
+    quotientList = []
+    dividend = 1*10
+    divisor  = n
+    reminder = 1
+    quotient = 1
+    while ( i <= 100 and reminder != 0):
+        while( dividend < divisor ):
+            dividend *= 10
+            quotientList.append('0')
 
-            for each recurringDigit:
-                if recurringDigit is part of dictionary key:
-                    return value against that digit
-                else
-                    for each keys in dictionary:
-                        append this recurring digit to values against those keys
-                    insert this new recurringDigit as key with value as recurringDigit
+        reminder = dividend%divisor
 
-            Okay so this yeilds 81 as answer if this is correct I'll go ahead and cleanup, BUT ... I'll do this only if this is correct
+        quotient = (dividend-reminder)/divisor
+        quotientList.append(str(quotient))
 
-    '''
-    while True:
-        d *= 10
-        r = d%n
-        recDigit =  (d-r)/n
-        dKey = recDigit%10
-        if dKey in rec.keys():
-            return rec[dKey]
-        else:
-            for key in rec.keys():
-                rec[key] = (rec[key]*10)+(dKey)
+        dividend  = (reminder*10)
+        i += 1
 
-            rec[dKey] = dKey
+    if reminder == 0:
+        return []
+    return ''.join(quotientList)
 
-def Solve():
-    seqList = []
-    for i in range(1,1000):
-        seq = recurringSequence(i)
-        if seq != 0:
-            seqList.append(seq)
-
-    seqList.sort()
-    print seqList[len(seqList) - 1]
-
-    return 0
+def Solve(sd):
+    largeSequenceGeneragedBy = 1
+    for d in range(1,sd):
+        rs = recurringSequence(d)
+        print "i = " , d , rs
 
 
 
 start_time = time.time()
-print Solve()
+print Solve(1000)
 print("--- %s seconds ---" % (time.time() - start_time))
+
