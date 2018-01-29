31122852a43e95bdbe9d28f1278e006512be27f3
Uday <k.udaykumar1985@gmail.com>
2018-01-29 19:31:37 +0530

    Better approach problem-1

--- problem-1.py
+++ problem-1.py
@@ -8,11 +8,20 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 import time
 
-def getMultiplesSum(n):
+def getMultiplesSum_Naive(n):
     sum = 0
     for n in range(n):
         if n%3 == 0 or n%5 == 0 :
             sum += n
+
+    return sum
+
+def getMultiplesSum(x):
+    sum = 0
+    n = (x-1)/3
+    sum += 3*n*(n+1)/2
+    n = (x-1)/5
+    sum += 5*n*(n+1)/2
 
     return sum
 
