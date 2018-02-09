77a6b52ee93babd20b5fc240cfff08291793403d
Uday <k.udaykumar1985@gmail.com>
2018-02-09 18:44:44 +0530

    Problem-40

---
+++ problem-40.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+An irrational decimal fraction is created by concatenating the positive integers:
+
+    0.123456789101112131415161718192021...
+
+    It can be seen that the 12th digit of the fractional part is 1.
+
+    If dn represents the nth digit of the fractional part, find the value of the following expression.
+
+    d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+
+'''
+
+import time
+import sys
+import math
+
+
+def Solve_Naive():
+    return 0
+
+def Solve():
+    return Solve_Naive()
+
+start_time = time.time()
+print Solve()
+print("--- %s seconds ---" % (time.time() - start_time))
+
