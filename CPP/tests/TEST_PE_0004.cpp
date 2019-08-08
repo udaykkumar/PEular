77e2911c13f445560a83116e96d1521ecc42e6fd
UK <k.udaykumar1985@gmail.com>
2019-08-08 09:19:32 +0530

    Compilation fix

--- CPP/tests/TEST_PE_0004.cpp
+++ CPP/tests/TEST_PE_0004.cpp
@@ -7,7 +7,7 @@ BOOST_AUTO_TEST_SUITE( TEST_PE_0004 )
 
 BOOST_AUTO_TEST_CASE( Case_1 )
 {
-	BOOST_CHECK( project_euler::PE_0004_Naive_Solution() == 906609 )
+	BOOST_CHECK( project_euler::PE_0004_Naive_Solution() == 906609 );
 }
 BOOST_AUTO_TEST_SUITE_END()
 
