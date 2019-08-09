5a81d70189d324c9374d7ebe6990ab71be31b1f3
Uday <k-uday.kumar@hpe.com>
2019-08-09 13:58:32 +0530

    switch to boost show have given it a try

--- CPP/src/PE_0055.cpp
+++ CPP/src/PE_0055.cpp
@@ -1,16 +1,32 @@
 #include <PE_0055.hpp>
+#include <boost/algorithm/is_palindrome.hpp>
+#include <boost/multiprecision/cpp_int.hpp> 
+using boost::multiprecision::cpp_int; 
 
 namespace project_euler
 {
+	size_t digits(cpp_int x)
+	{
+		size_t n_digits = 0;
+		for( ; x ; x /= 10 )
+		{
+			n_digits ++;
+		}
+
+		return n_digits;
+	}
+
 	size_t non_lychrel_nos()
 	{
-		large_int n1(1), n2(2);
+		cpp_int n1 = 1;
 
-		while( 1 )
+		size_t iterations = 0;
+		while( iterations++ < 1000 )
 		{
-			n1 = n1 + n1 ;
-			std::cout << n1.get() << " \n";
+			n1 += n1;
 		}
+
+		return iterations;
 	}
 
 }
--- CPP/src/PE_0055.hpp
+++ CPP/src/PE_0055.hpp
@@ -5,48 +5,6 @@
 
 namespace project_euler
 {
-	struct large_int
-	{
-	private:
-		int num[1024];
-		size_t numLen;
-
-	public:
-		large_int(const int n)
-		{
-			for( numLen = 0 ; n /= 10 )
-			{
-				num[ numLen++] = n%10 ;
-			}
-		}
-
-		large_int& operator +(const large_int& n1)
-		{
-			int carry = 0;
-			for( size_t i = 0, j = 0 ; 
-				i < numLen && j < n1.length() ; i ++, j ++ )
-			{
-				num[i] = (carry + digit_at(i) + n1.digit_at(j))%10;
-				carry  = (carry + digit_at(i) + n1.digit_at(j))/10;
-			}
-		}
-
-		int digit_at(size_t i) {
-			return num[i];
-		}
-
-		std::string get() const { 
-			std::string s;
-			for( size_t i = numLen; i >= 0 ; i -- )
-				s.append( s.to_string(num[i]));
-			return s;
-		}
-
-		size_t length() const { return numLen; }
-
-
-	};
-
 	size_t non_lychrel_nos();
 }
 
